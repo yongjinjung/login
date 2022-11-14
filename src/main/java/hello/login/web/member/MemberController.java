4d683d253cdd3d74ec6b3a7652117ad171dd9f06
YongJin Jung <jyj1641@gmail.com>
2022-11-14 17:04:47 +0900

    login

---
+++ src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,41 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member){
+
+        log.info("member = {}" , member);
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Validated @ModelAttribute("member") Member member, BindingResult result){
+
+        if(result.hasErrors()){
+            return "members/addMemberForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+
+    }
+}
